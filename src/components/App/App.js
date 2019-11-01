18ebe4e68e82c886486c7d09ff75c90eb04a99ae
DarkChambers <monteilchristophe@live.fr>
2019-11-01 11:32:29 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../elements/Header/Header'
+import Home from '../Home/Home'
+
+//entryPoint of the application, stateless component
+const App = () => {
+
+    return (
+        <div>
+            <Header />
+            <Home />
+        </div>
+    )
+}
+
+export default App
